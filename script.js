9369ba9745886891e96732683bcb8910153b3397
yablony <yablonskaya.yuliya@gmail.com>
2023-04-21 11:43:06 +0930

    Added code to make X and O alternate

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var grid = document.querySelector('section');
 
 function gameTurns(event) {
-    var square = event.target; 
+    var square = event.target;
+    var crossSquare = document.querySelectorAll('div.cross');
+    var noughtSquare = document.querySelectorAll('div.nought');
 
-    if (square.tagName === 'DIV')
-    square.classList.add('cross');
-}
+    if (square.tagName === 'DIV' && square.className === '') {
+             if (crossSquare.length > noughtSquare.length) {
+                square.classList.add('nought');
+            } else {
+                square.classList.add('cross');
+            }
+        }
+    }
 
 grid.addEventListener('click', gameTurns);
